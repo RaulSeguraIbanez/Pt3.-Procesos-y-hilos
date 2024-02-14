25268867ccd8af5fd1b5af77fd7f9a83f6808b6f
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2024-02-14 18:46:48 +0100

    Update leerFichero.java

--- pt3_procesos_hilos/leerFichero.java
+++ pt3_procesos_hilos/leerFichero.java
@@ -1,5 +1,12 @@
 package pt3_procesos_hilos;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class leerFichero {
 	public String fichero_txt = "fichero_txt";
+	
+	public void leerFichero() {
+		
+	}
 }
